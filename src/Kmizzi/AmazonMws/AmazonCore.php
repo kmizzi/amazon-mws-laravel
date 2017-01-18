71b0458dd950e28ebd136f4ab065bf4e6fdb911f
Rob Vella <me@robvella.com>
2017-01-18 13:56:33 -0800

    Default log level debug

--- src/Kmizzi/AmazonMws/AmazonCore.php
+++ src/Kmizzi/AmazonMws/AmazonCore.php
@@ -475,7 +475,7 @@ abstract class AmazonCore
      * @return boolean <b>FALSE</b> if the message is empty, NULL if logging is muted
      * @throws Exception If the file can't be written to.
      */
-    protected function log($msg, $level = 'Info')
+    protected function log($msg, $level = 'Debug')
     {
         if ($msg != false) {
             $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
